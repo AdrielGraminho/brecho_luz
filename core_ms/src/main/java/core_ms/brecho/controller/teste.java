bc723fe7cb2789503f24f93580c82f810f0a9954
AdrielGraminho <adrielgraminho@gmail.com>
2022-01-02 14:17:34 -0300

    add autenticate by role in ms core

--- core_ms/src/main/java/core_ms/brecho/controller/teste.java
+++ core_ms/src/main/java/core_ms/brecho/controller/teste.java
@@ -1,6 +1,7 @@
 package core_ms.brecho.controller;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/teste")
 public class teste {
     @GetMapping
+    @PreAuthorize("hasAnyAuthority('ROLE_USER')")
     public HttpStatus getTest(){
         return HttpStatus.ACCEPTED;
     }
